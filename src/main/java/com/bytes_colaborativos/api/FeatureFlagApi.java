f30b5390b07852b2eab1548b556671e9285120bf
dio-quincar <dio-quincar@outlook.com>
2025-09-05 11:21:50 -0500

    Initial commit: project structure and basic dependencies

---
+++ src/main/java/com/bytes_colaborativos/api/FeatureFlagApi.java
@@ -0,0 +1,13 @@
+package com.bytes_colaborativos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagApi.class, args);
+	}
+
+}
